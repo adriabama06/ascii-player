d52102dca22d6b126a0466dd3a15992e9a74ce28
adriabama06 <adriabama06@gmail.com>
2022-10-12 21:56:36 +0200

    ok, in windows the time work 100%

--- src/main.c
+++ src/main.c
@@ -24,9 +24,9 @@ int main(int argc, const char* argv[])
     PROGRAM_USER_INPUT options = parseArguments(argc, argv);
 
     #ifdef _WIN32
-        uint32_t ms_frame_delay = 1000 / options.fps;
+        uint32_t frame_delay = 1000000 / options.fps;
     #else
-        uint32_t ms_frame_delay = 1000000 / options.fps;
+        uint32_t frame_delay = 1000000 / options.fps;
 
     #endif
 
@@ -63,17 +63,25 @@ int main(int argc, const char* argv[])
         clock_t start = clock();
     #endif
 
+    uint32_t last_i = 0;
+
     while (i < txt_files->length)
     {
         #ifdef _WIN32
             GetSystemTime(&current);
 
-            i = ((calc_win_time_to_ms(current)) - start_pre_calc) / ms_frame_delay;
+            i = (((calc_win_time_to_ms(current)) - start_pre_calc) * 1000) / frame_delay;
         #else
             clock_t current = clock();
             
-            i = (current - start) / ms_frame_delay;
+            i = (current - start) / frame_delay;
         #endif
+
+        //if(i != last_i)
+        //{
+        //    last_i = i;
+        //    printf("%d - %d = %d / %d => %d\n", calc_win_time_to_ms(current), start_pre_calc, (calc_win_time_to_ms(current) - start_pre_calc), frame_delay, i);
+        //}
     }
     
     end = 1;
